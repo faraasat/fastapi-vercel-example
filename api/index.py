81ad3637c017482077b55d13b53e34c3e04f7e45
Farasat Ali <fali@digital-dividend.com>
2023-12-27 13:54:02 +0500

    updated

--- api/index.py
+++ api/index.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {
-        "message": "Welcome to my notes application, use the /docs route to proceed"
-    }
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
